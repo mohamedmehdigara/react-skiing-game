be5934ca8a9f7fe5d68ea957e47f737ec0325766
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-20 10:21:46 +0100

    added files

--- src/components/Obstacle.js
+++ src/components/Obstacle.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const obstacleAnimation = keyframes`
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(100%);
+  }
+`;
 
 const ObstacleContainer = styled.div`
   position: absolute;
   width: 30px;
   height: 30px;
-  background-color: #ff0000;
   left: ${(props) => props.position.x}px;
   top: ${(props) => props.position.y}px;
+  animation: ${obstacleAnimation} 3s linear infinite;
 `;
 
+const ObstacleSvg = () => (
+  <svg
+    width="100%"
+    height="100%"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+  >
+    {/* Obstacle shape (e.g., a tree) */}
+    <rect x="40" y="20" width="20" height="40" fill="#ff0000" />
+  </svg>
+);
+
 const Obstacle = ({ position }) => {
-  return <ObstacleContainer position={position}></ObstacleContainer>;
+  return (
+    <ObstacleContainer position={position}>
+      <ObstacleSvg />
+    </ObstacleContainer>
+  );
 };
 
 export default Obstacle;
--- src/components/Skier.js
+++ src/components/Skier.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const skiingAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 const SkierContainer = styled.div`
   position: absolute;
   width: 40px;
   height: 40px;
-  background-color: #00ff00;
   left: ${(props) => props.position.x}px;
   top: ${(props) => props.position.y}px;
+  animation: ${skiingAnimation} 1s linear infinite;
 `;
 
+const SkierSvg = () => (
+  <svg
+    width="100%"
+    height="100%"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+  >
+    {/* Skier body */}
+    <circle cx="50" cy="35" r="15" fill="#00ff00" />
+
+    {/* Skier legs */}
+    <line x1="50" y1="50" x2="40" y2="60" stroke="#00ff00" strokeWidth="2" />
+    <line x1="50" y1="50" x2="60" y2="60" stroke="#00ff00" strokeWidth="2" />
+
+    {/* Skier head */}
+    <circle cx="50" cy="30" r="5" fill="#f00" />
+  </svg>
+);
+
 const Skier = ({ position }) => {
-  return <SkierContainer position={position}></SkierContainer>;
+  return (
+    <SkierContainer position={position}>
+      <SkierSvg />
+    </SkierContainer>
+  );
 };
 
 export default Skier;
